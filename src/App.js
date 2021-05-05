81f331a66b7e3506876966d5b5759c92c0b73aa6
busrakaraa <busraakra@gmail.com>
2021-05-05 23:21:15 +0300

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const themes = {
+  light: {
+    foreground: '#000000',
+    background: '#eeeeee',
+
+  },
+  dark : {
+    foreground: '#ffffff',
+    background: '#222222',
+  },
+}  
+
+//1. Create context
+const ThemeContext = React.createContext(themes.dark);
+
+//2. Create Provider
+//3. Create Consumer to use the data
+
+const ThemedButton = () => {
+  return(
+    <ThemeContext.Consumer>
+      {
+          value => (
+            <button>Themed button ! {value.background}</button>
+          )
+      }
+   
+    </ThemeContext.Consumer>
+   
   );
 }
+
+const Toolbar = () => (
+  <ThemedButton/>
+)
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <ThemeContext.Provider value={themes.dark}>
+          <Toolbar />
+        </ThemeContext.Provider>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
